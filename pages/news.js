ec93fa019c10644287336b03e2c061284e2e49e9
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-22 18:00:00 -0700

    created new index and news pages

---
+++ pages/news.js
@@ -0,0 +1,8 @@
+// route: our-domain.com/news, the file name will be the route name
+
+// standard React component goes here
+const NewsPage = () => {
+    return <h1>Welcome to Yu-Chun News Page!</h1>
+};
+
+export default NewsPage;
